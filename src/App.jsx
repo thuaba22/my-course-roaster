b1b122c5c3de980e08091d91b6f11a41a4506d7a
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-09-15 17:43:40 +0600

    created all the necessary components and jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-[#1C1B1B]  text-[32px] font-bold text-center mt-12 mb-8">
+        Course Registration
+      </h1>
     </>
   );
 }
