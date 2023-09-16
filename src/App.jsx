332e2ea3d5b15e9532c7eec1ad8b64e01be0fc79
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-09-16 10:20:27 +0600

    added toast messages using react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import Cards from "./components/Cards/Cards";
 import Cart from "./components/Cart/Cart";
-import Toast from "./components/Toast/Toast";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [selectCourses, setSelectCourses] = useState([]);
-  const [toastMessage, setToastMessage] = useState("");
   const handleSelectCourse = (course) => {
     const duplicateCourses = selectCourses.filter(
       (c) => c.course_id === course.course_id
     )[0];
     if (duplicateCourses) {
-      setToastMessage("You can not add the same course again");
+      toast("You can not add the same course again");
       return;
     }
 
     const newSelectCourses = [...selectCourses, course];
     setSelectCourses(newSelectCourses);
-
-    // clearing the toast message
-    setTimeout(() => {
-      setToastMessage("");
-    }, 3000);
   };
   return (
     <>
@@ -32,9 +27,7 @@ function App() {
       <main className="flex max-w-[1180px] mx-auto mb-[80px]">
         <Cards handleSelectCourse={handleSelectCourse}></Cards>
         <Cart selectCourses={selectCourses}></Cart>
-        {toastMessage && (
-          <Toast message={toastMessage} keyProp={new Date().getTime()}></Toast>
-        )}
+        <ToastContainer />
       </main>
     </>
   );
