872c9a067f20d4a874562df24d1bb6f865ee687a
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-09-16 11:16:16 +0600

    completed the total creadit,remaining credit,total price functionality and also added toast messages to them

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [selectCourses, setSelectCourses] = useState([]);
+  const [totalCredits, setTotalCredits] = useState(0);
+  const [remainingCredits, setRemainingCredits] = useState(20);
+  const [totalPrice, setTotalPrice] = useState(0);
   const handleSelectCourse = (course) => {
     const duplicateCourses = selectCourses.filter(
       (c) => c.course_id === course.course_id
@@ -15,9 +18,22 @@ function App() {
       toast("You can not add the same course again");
       return;
     }
+    if (totalCredits + course.credit_hours > 20) {
+      toast("You can't add more than 20 credits.");
+      toast("Remaining credit hour can't be less than 0");
+
+      return;
+    }
+    if (20 - (totalCredits + course.credit_hours) < 0) {
+      toast("Remaining credit hour can't be less than 0");
+      return;
+    }
 
     const newSelectCourses = [...selectCourses, course];
     setSelectCourses(newSelectCourses);
+    setTotalCredits(totalCredits + course.credit_hours);
+    setRemainingCredits(20 - (totalCredits + course.credit_hours));
+    setTotalPrice(totalPrice + course.price);
   };
   return (
     <>
@@ -26,7 +42,12 @@ function App() {
       </h1>
       <main className="flex max-w-[1180px] mx-auto mb-[80px]">
         <Cards handleSelectCourse={handleSelectCourse}></Cards>
-        <Cart selectCourses={selectCourses}></Cart>
+        <Cart
+          selectCourses={selectCourses}
+          totalCredits={totalCredits}
+          remainingCredits={remainingCredits}
+          totalPrice={totalPrice}
+        ></Cart>
         <ToastContainer />
       </main>
     </>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import CartElement from "../CartElement/CartElement";
 
-const Cart = ({ selectCourses }) => {
+const Cart = ({
+  selectCourses,
+  totalCredits,
+  remainingCredits,
+  totalPrice,
+}) => {
   return (
     <div className="bg-white p-4 ml-6 h-[100%] rounded-xl">
-      <h3 className="text-[#2F80ED] text-base font-bold mb-4">
-        Credit Hour Remaining: hr
-      </h3>
+      <h5 className="text-[#2F80ED] text-xs font-bold mb-4">
+        Credit Hour Remaining: {remainingCredits}hr
+      </h5>
       <hr />
       <h3 className="text-[#1C1B1B] text-xl font-bold mt-4 mb-5">
         Course Name
@@ -16,16 +21,19 @@ const Cart = ({ selectCourses }) => {
       ))}
       <hr />
       <p className="text-[#1C1B1BCC] font-medium text-sm mt-4 mb-4">
-        Total Credit Hour :
+        Total Credit Hour : {totalCredits}
       </p>
       <hr />
       <p className="text-[#1C1B1BCC] font-medium text-sm mt-4 mb-4">
-        Total Price :{" "}
+        Total Price : {totalPrice}$
       </p>
     </div>
   );
 };
 Cart.propTypes = {
   selectCourses: PropTypes.array,
+  totalCredits: PropTypes.number,
+  remainingCredits: PropTypes.number,
+  totalPrice: PropTypes.number,
 };
 export default Cart;
